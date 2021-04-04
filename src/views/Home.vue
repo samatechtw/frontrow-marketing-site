5bc27f7cb2d58dc684912c3eebf2e611fd08454b
Samatech <samatechtw@gmail.com>
2021-04-04 12:12:51 +0800

    Disable contact section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,9 @@
   <Risks ref="risks" :selected="activeSection === 'risks'" />
   <Token ref="token" :selected="activeSection === 'token'" />
   <Faq ref="faq" :selected="activeSection === 'faq'" />
+  <!--
   <Contact />
+  -->
   <TpaFooter />
 </div>
 </template>
