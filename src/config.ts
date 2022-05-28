3a2aba498f773b76dcbc414eec908f690f95edb5
Sam Pullman <sampullman@gmail.com>
2022-05-29 00:05:18 +0800

    update whitepaper

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const WhitepaperEng = '/FRR_Whitepaper_ENG_220511.pdf';
+export const WhitepaperEng = '/FRR_Whitepaper_ENG_5-25-22.pdf';
 export const OnepagerEng = '/210426_TPA_FINANCE_ENG.pdf';
 
 export const WhitepaperKor = '/FRR_Whitepaper_KOR_211204.pdf';
